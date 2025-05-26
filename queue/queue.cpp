b44bd5d9d7caa1e40da9a15402b7627591fe8982
Dohyunsoo <godwnswkd@gmail.com>
2025-05-26 18:53:01 +0900

    queue 기본 동작 구(락x)

---
+++ queue/queue.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include "queue.h"
+
+
+Queue* init(void) {
+	Queue* q = new Queue;
+	q->head = nullptr;
+	q->tail = nullptr;
+	return q;
+}
+
+
+void release(Queue* queue) {
+	if (!queue) return;
+	Node* curr = queue->head;
+	while (curr) {
+		Node* next = curr->next;
+		nfree(curr);
+		curr = next;
+	}
+	delete queue;
+}
+
+
+Node* nalloc(Item item) {
+	Node* node = new Node;
+	node->item = item;
+	node->next = nullptr;
+	return node;
+}
+
+
+void nfree(Node* node) {
+	delete node;
+}
+
+
+Node* nclone(Node* node) {
+	if (!node) return nullptr;
+	return nalloc(node->item);
+}
+
+
+Reply enqueue(Queue* queue, Item item) {
+	Node* new_node = nalloc(item);
+	if (!new_node) return { false, item };
+
+	if (!queue->head) {
+		queue->head = queue->tail = new_node;
+		return { true, item };
+	}
+
+	Node* prev = nullptr;
+	Node* curr = queue->head;
+	while (curr && curr->item.key > item.key) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	if (!prev) {
+		new_node->next = queue->head;
+		queue->head = new_node;
+	}
+	else {
+		prev->next = new_node;
+		new_node->next = curr;
+	}
+
+	if (!curr) queue->tail = new_node;
+	return { true, item };
+}
+
+Reply dequeue(Queue* queue) {
+	if (!queue->head) return { false, {} };
+
+	Node* node = queue->head;
+	queue->head = node->next;
+	if (!queue->head) queue->tail = nullptr;
+
+	Item item = node->item;
+	nfree(node);
+	return { true, item };
+}
+
+Queue* range(Queue* queue, Key start, Key end) {
+	Queue* new_q = init();
+	if (!new_q) return nullptr;
+
+	Node* curr = queue->head;
+	while (curr) {
+		if (start <= curr->item.key && curr->item.key <= end) {
+			enqueue(new_q, curr->item);
+		}
+		curr = curr->next;
+	}
+
+	return new_q;
+}
